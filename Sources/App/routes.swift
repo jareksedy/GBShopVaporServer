ade52f152aa1025f64100271c01ba8cc608d5139
Jarek Šedý <jareksedy@icloud.com>
2021-12-10 22:20:18 +0600

    Testing vapor server.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
+        return "TEST GBSHOP VAPOR SERVER."
     }
 
     app.get("hello") { req -> String in
