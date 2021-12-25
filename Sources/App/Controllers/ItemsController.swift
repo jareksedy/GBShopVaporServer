423755a69a975498f9d4f75edfe41b67e279d0b3
Jarek Šedý <jareksedy@icloud.com>
2021-12-25 14:59:03 +0600

    Added catalog & iterm responses.

--- Sources/App/Controllers/ItemsController.swift
+++ Sources/App/Controllers/ItemsController.swift
@@ -14,9 +14,10 @@ class ItemsController {
         print(body)
         
         let response = [
-        CatalogResponse(productId: 242, productName: "Эппл мэджик мышъ", price: 100500, shortDescription: nil, picUrl: nil),
-        CatalogResponse(productId: 243, productName: "Эппл ойфонъ", price: 100600, shortDescription: nil, picUrl: nil),
-        CatalogResponse(productId: 244, productName: "Эппл ойпадъ", price: 100666, shortDescription: nil, picUrl: nil)
+            CatalogResponse(productId: 123, productName: "Эппл тряпочка", price: 1990, shortDescription: "Тряпочка Эппл протирать всякое.", picUrl: "https://i.imgur.com/WzgFAPe.jpg"),
+            CatalogResponse(productId: 124, productName: "Магическая эппл-мыш", price: 7900, shortDescription: "Мыш Мэджик Маус Эппл магическая.", picUrl: "https://i.imgur.com/zc2ksyh.jpg"),
+            CatalogResponse(productId: 125, productName: "Наушники эйрподы про", price: 24990, shortDescription: "Стильные, модные, молодежные наушники эппл.", picUrl: "https://i.imgur.com/F0ZHN9T.jpg"),
+            CatalogResponse(productId: 126, productName: "Эппл Эйртаг одна штука", price: 2990, shortDescription: "Позволяет следить за людьми.", picUrl: "https://i.imgur.com/8T965iA.jpg")
         ]
         
         return req.eventLoop.future(response)
@@ -27,7 +28,15 @@ class ItemsController {
         
         print(body)
         
-        let response = GoodResponse(result: 1, price: 100500, description: "Большая красивая Эппл магическая мыш!")
+        var response = GoodResponse(result: 1, productId: 999, productName: "Неизвестный товар.", price: 0, description: "Неизвестный товар, непонятный.", picUrl: nil)
+        
+        switch body.productId {
+        case 123: response = GoodResponse(result: 1, productId: 123, productName: "Эппл тряпочка", price: 1990, description: "Тряпочка Эппл протирать всякое.", picUrl: "https://i.imgur.com/WzgFAPe.jpg")
+        case 124: response = GoodResponse(result: 1, productId: 124, productName: "Магическая эппл-мыш", price: 7900, description: "Мыш Мэджик Маус Эппл магическая.", picUrl: "https://i.imgur.com/zc2ksyh.jpg")
+        case 125: response = GoodResponse(result: 1, productId: 125, productName: "Наушники эйрподы про", price: 24990, description: "Стильные, модные, молодежные наушники эппл.", picUrl: "https://i.imgur.com/F0ZHN9T.jpg")
+        case 126: response = GoodResponse(result: 1, productId: 126, productName: "Эппл Эйртаг одна штука", price: 2990, description: "Позволяет следить за людьми.", picUrl: "https://i.imgur.com/8T965iA.jpg")
+        default: response = GoodResponse(result: 1, productId: 999, productName: "Неизвестный товар.", price: 0, description: "Неизвестный товар, непонятный.", picUrl: nil)
+        }
         
         return req.eventLoop.future(response)
     }
--- Sources/App/Models/GoodResponse.swift
+++ Sources/App/Models/GoodResponse.swift
@@ -9,6 +9,9 @@ import Vapor
 
 struct GoodResponse: Content {
     let result: Int?
+    let productId: Int?
+    let productName: String?
     let price: Int?
     let description: String?
+    let picUrl: String?
 }
